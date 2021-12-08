ce4db1da7d29fa27c18e1dcdee3ac50552ee2c2a
StarBoyAbhi <abhi@StarBoy.com>
2021-12-08 19:42:41 +0530

    printing numbers in both orders

--- one.py
+++ one.py
@@ -1,10 +1,18 @@
 #printing 5 numbers from beginning using recursion
 
-def num_upto(num):
-    if num>0:
-        print(num_upto(num-1) + 1)
-        return num
-    else:
-        return 1
 
-num_upto(10)
+def num_upto(num, switch):
+    if num == 1:
+        print(num)
+        switch = 1
+        num_upto(num+1, switch)
+    elif num == 4 and switch == 1:
+        print(num)
+    else:
+        print(num)
+        if switch == 0:
+            num_upto(num-1, switch)
+        else:
+            num_upto(num+1, switch)
+
+num_upto(4, 0)
