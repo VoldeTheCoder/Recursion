9074b922d67cd138543a6b33e8bf438ec8fb7127
StarBoyAbhi <abhi@StarBoy.com>
2021-12-08 19:15:37 +0530

    changed code to print numbers in reverse order

--- one.py
+++ one.py
@@ -1,8 +1,10 @@
-#printing 5 numbers using recursion
+#printing 5 numbers from beginning using recursion
 
 def num_upto(num):
     if num>0:
-        print(num)
-        return num_upto(num-1)
+        print(num_upto(num-1) + 1)
+        return num
+    else:
+        return 1
 
 num_upto(10)
